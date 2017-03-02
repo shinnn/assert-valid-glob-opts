b2c10abd183e9f44931acb19cb0fea024c5ef2b0
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-03-02 19:46:59 +0900

    use const and template literal
    [BREAKING] drop support for Node < 4

--- index.js
+++ index.js
@@ -4,19 +4,19 @@
 */
 'use strict';
 
-var validateGlobOpts = require('validate-glob-opts');
+const validateGlobOpts = require('validate-glob-opts');
 
 function isTypeError(err) {
   return err.name === 'TypeError';
 }
 
 function createMessageLine(msg, err, index) {
-  return msg + '\n  ' + (index + 1) + '. ' + err.message;
+  return `${msg}\n  ${index + 1}. ${err.message}`;
 }
 
 module.exports = function assertValidGlobOpts(obj) {
-  var results = validateGlobOpts(obj);
-  var count = results.length;
+  const results = validateGlobOpts(obj);
+  const count = results.length;
 
   if (count === 0) {
     return;
@@ -28,6 +28,6 @@ module.exports = function assertValidGlobOpts(obj) {
 
   throw new (results.every(isTypeError) ? TypeError : Error)(results.reduce(
     createMessageLine,
-    String(count) + ' errors found in the node-glob options:'
+    `${count} errors found in the node-glob options:`
   ));
 };
